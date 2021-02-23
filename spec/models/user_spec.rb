67a3d621dc4028e060646c1ac9badadf77535a10
Daham94 <daham@team.mindvision.com.au>
2021-02-23 20:33:23 +0530

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validation tests' do
-    it 'ensures name presence' do
-      user=User.new(email: 'example@gmail.com').save
-      expect(user).to eq(false)
-    end
+  before(:each) do
+    @user=User.create!(name: "Test Name", email: "email@gmail.com",
+                        password: "foobar", password_confirmation: "foobar")
+  end
 
-    it 'ensures name presence' do
-      user=User.new(name: 'Name').save
-      expect(user).to eq(false)
+  describe "validations" do
+    it "name should be presence" do
+      @user.name=nil
+      expect(@user).to_not be_valid
     end
-
-    it 'should save successfully' do
-      user=User.new(name: 'Name', email: 'example@gmail.com').save
-      expect(user).to eq(true)
+    it "email should be presence" do
+      @user.email=nil
+      expect(@user).to_not be_valid
     end
+    it "password should be presence" do
+      @user.password=nil
+      expect(@user).to_not be_valid
+    end
+  end
 
+  describe "maximum length validation" do
+    it "name should not be longer than 50 " do
+      @user.name="j"*51
+      expect(@user).to_not be_valid
+    end
+    it "email should not be too long " do
+      @user.email="a" * 244 + "@example.com"
+      expect(@user).to_not be_valid
+    end
+  end
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@example,com user_at_foo.org user.name@example.
+                  foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+            @user.email = invalid_address
+            expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                  first.last@foo.jp alice+bob@baz.cn]
+      addresses.each do |valid_address|
+            @user.email = valid_address
+            expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email is unique" do
+    it "should be valid" do
+      duplicate_user = @user.dup
+      expect(@user).to be_valid
+    end
+  end
+
+  describe "minimum password length validation" do
+    it "should not be blank" do
+      @user.password=" "*6
+      expect(@user).to_not be_valid
+    end
+    it "should not be too short" do
+      @user.password=@user.password_confirmation="a" * 5
+      expect(@user).to_not be_valid
+    end
   end
 end
