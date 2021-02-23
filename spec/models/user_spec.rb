84645dd92a0aeaa7fced7a123ff0ec5f425b34a1
Daham94 <daham@team.mindvision.com.au>
2021-02-23 12:52:12 +0530

    Finish static pages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'ensures name presence' do
+      user=User.new(email: 'example@gmail.com').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures name presence' do
+      user=User.new(name: 'Name').save
+      expect(user).to eq(false)
+    end
+
+    it 'should save successfully' do
+      user=User.new(name: 'Name', email: 'example@gmail.com').save
+      expect(user).to eq(true)
+    end
+
+  end
 end
